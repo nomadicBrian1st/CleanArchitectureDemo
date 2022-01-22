931bbf2fcf2bac23ec78b86cf4328ceb0fa3dbd9
Brian Quinn <brian.quinn@amtote.com>
2022-01-21 23:58:19 -0500

    Service & Repository Interfaces

---
+++ CleanArch/ICourseRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using CleanArch.Domain.M
+
+
+namespace CleanArch.Domain.Interfaces
+{
+    public interface ICourseRepository
+    {
+        IEnumerable<Course> GetCourse();
+    }
+}
